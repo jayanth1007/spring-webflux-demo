c278a13fdf00600d7e7ed62824539b6a4e9df418
Jayanthi Kolli <jayanthi.babu.kolli@macys.com>
2019-03-18 11:33:57 -0700

    Initial commit

---
+++ src/main/java/com/jayanthi/demo/EmployeeReactiveController.java
@@ -0,0 +1,36 @@
+package com.jayanthi.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeReactiveController {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeReactiveController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/{id}")
+    private Mono<Employee> getEmployeeById(@PathVariable String id) {
+        return employeeRepository.findEmployeeById(id);
+    }
+
+    @GetMapping
+    private Flux<Employee> getAllEmployees() {
+        return employeeRepository.findAllEmployees();
+    }
+
+    @PostMapping("/update")
+    private Mono<Employee> updateEmployee(@RequestBody Employee employee) {
+        return employeeRepository.updateEmployee(employee);
+    }
+}
---
+++ src/main/java/com/jayanthi/demo/EmployeeSpringApplication.java
@@ -0,0 +1,17 @@
+package com.jayanthi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmployeeSpringApplication.class, args);
+
+        EmployeeWebClient employeeWebClient = new EmployeeWebClient();
+        employeeWebClient.consume();
+    }
+
+}
